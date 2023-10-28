428ea395922c90f87f520984a606b62651438fac
narakrit phongsamut <black@Macintosh.local>
2023-10-29 01:51:05 +0700

    finish

--- crosswordhacker.js
+++ crosswordhacker.js
@@ -26,18 +26,20 @@ function hand(word) {
   return letterCount;
 }
 
-let hands = [
-  { alphabet: "a", count: 1 },
-  { alphabet: "b", count: 1 },
-  { alphabet: "c", count: 1 },
-  { alphabet: "d", count: 1 },
-  { alphabet: "e", count: 1 },
-  { alphabet: "f", count: 1 },
-  { alphabet: "g", count: 1 },
-  { alphabet: "h", count: 1 }
-];
+// let hands = [
+//   { alphabet: "a", count: 1 },
+//   { alphabet: "b", count: 1 },
+//   { alphabet: "c", count: 1 },
+//   { alphabet: "d", count: 1 },
+//   { alphabet: "e", count: 1 },
+//   { alphabet: "f", count: 1 },
+//   { alphabet: "g", count: 1 },
+//   { alphabet: "h", count: 1 }
+// ];
+let input = prompt()
+const hands = hand(input)
 
-const final = [];
+const finals = [];
 for (let i = 0; i < newWords.length; i++) {
   let count = 0;
   for (let j = 0; j < newWords[i].length; j++) {
@@ -52,10 +54,23 @@ for (let i = 0; i < newWords.length; i++) {
       }
     }
     if (count === newWords[i].length) {
-      final.push(plainWords[i]);
+      finals.push(plainWords[i]);
     }
   }
 }
 
-// let hands = prompt()
-console.log(final.length);
+const findMaxs = []
+for (const final of finals) {
+    findMaxs.push(final.length)
+}
+
+const findMax = findMaxs.sort()[findMaxs.length-1]
+for (let i = findMax ; i > 0 ; i = i - 1) {
+    const allWords = []
+    for (const final of finals) {
+        if (final.length === i) {
+            allWords.push(final)
+        }
+    }
+    console.log("alphabetLength" + i + ":" , allWords) //note: เขียนวิธีอื่นได้ไหม?
+}
